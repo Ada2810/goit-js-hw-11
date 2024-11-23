000a3a9e8535558b13b53584242b4ce7c371864d
Ada2810 <ade.cozma@gmail.com>
2024-11-23 23:36:59 +0200

    modified 1

--- src/index.js
+++ src/index.js
@@ -65,18 +65,24 @@ function renderGallery(images) {
   const markup = images
     .map(
       ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-    <a href="${largeImageURL}" class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item"><b>Likes</b> ${likes}</p>
-        <p class="info-item"><b>Views</b> ${views}</p>
-        <p class="info-item"><b>Comments</b> ${comments}</p>
-        <p class="info-item"><b>Downloads</b> ${downloads}</p>
-      </div>
-    </a>`
+        <a href="${largeImageURL}" class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item"><b>Likes</b> ${likes}</p>
+            <p class="info-item"><b>Views</b> ${views}</p>
+            <p class="info-item"><b>Comments</b> ${comments}</p>
+            <p class="info-item"><b>Downloads</b> ${downloads}</p>
+          </div>
+        </a>
+      `
     )
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
-  new SimpleLightbox('.gallery a').refresh();
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lightbox.refresh();
 }
