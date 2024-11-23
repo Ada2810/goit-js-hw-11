d8cde1b9b53cc64216727c2db6eb7e6da205fa16
Ada2810 <ade.cozma@gmail.com>
2024-11-23 23:53:43 +0200

    modified render again

--- src/index.js
+++ src/index.js
@@ -62,25 +62,13 @@ async function fetchAndRenderImages() {
 function renderGallery(images) {
   const markup = images
     .map(
-      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-        <a href="${largeImageURL}" class="photo-card">
+      ({ webformatURL, tags }) => `
+        <div class="photo-card">
           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item"><b>Likes</b> ${likes}</p>
-            <p class="info-item"><b>Views</b> ${views}</p>
-            <p class="info-item"><b>Comments</b> ${comments}</p>
-            <p class="info-item"><b>Downloads</b> ${downloads}</p>
-          </div>
-        </a>
+        </div>
       `
     )
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
-
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
-  lightbox.refresh();
 }
